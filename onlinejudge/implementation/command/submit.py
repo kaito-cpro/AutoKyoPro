6905b66743c50bc4e0dd39ba418efde5e072e0d6
kaito <blue7-go0112@docomo.ne.jp>
2020-04-05 23:25:50 +0900

    update: judge system language update

--- onlinejudge/implementation/command/submit.py
+++ onlinejudge/implementation/command/submit.py
@@ -284,6 +284,9 @@ def guess_lang_ids_of_file(filename: pathlib.Path, code: bytes, language_dict, c
             for version in versions:
                 lang_ids += select('python%d'  % version, saved_ids)
                 lang_ids += select('python %d' % version, saved_ids)
+                # 自分で書き換えた箇所(AtCoder の judge で言語がアップデートされたら追加する)
+                lang_ids += select('python(%d' % version, saved_ids)
+                lang_ids += select('python (%d' % version, saved_ids)
 
         lang_ids = list(set(lang_ids))
         return lang_ids
